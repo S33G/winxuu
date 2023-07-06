c174fd80d50f047c7a612b7ca10c6915bbd55a9b
CJ <dev@charlie.fyi>
2023-07-06 11:44:37 +0000

    Fix

--- src/WinXUU.tsx
+++ src/WinXUU.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './winxuu.scss'
 import { AiFillWindows } from 'react-icons/ai'
 import Template from './Template'
